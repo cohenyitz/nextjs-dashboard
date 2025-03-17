eea57c0c7dea746382eb971c770a67a1d43c8c04
Yitzhak Cohen <cohenyitz@gmail.com>
2025-03-17 01:42:13 -0400

    adding NextAuth for authentication and added login, logout, and protected routes. Also added metadata and cleaned up some code.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import {z} from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
     errors?: {
@@ -23,6 +25,27 @@ const FormSchema = z.object({
     status: z.enum(['pending', 'paid'], {invalid_type_error: 'Please select an invoice status.'}),
     date: z.string(),
 });
+
+
+
+/**
+ * Authenticate user
+ */
+export async function authenticate(prevState:string | undefined, formData: FormData) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if(error instanceof AuthError){
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+};
 
 
 
